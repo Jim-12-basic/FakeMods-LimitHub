5b68f66ca518a4a80113b9b2965042cb637b13f2
TIMBERWOLVES <masteredge454@gmail.com>
2025-07-22 18:36:39 +0800

    Update and rename EHU to Limithub_Luamor_E.lua

--- Limithub_Luamor_E.lua
+++ Limithub_Luamor_E.lua
@@ -1,2 +1,2 @@
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/wJRyd3q2Ov0f3bmf/raw", true))()
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/grgV8smvEF/0", true))()
